2aa5fe9201168d15b0d03320dc385d9d36f8bc6c
Till Macher <48299746+TillMacher@users.noreply.github.com>
2025-08-22 12:59:21 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="apscale_installer",
-    version="0.4",
+    version="0.5",
     author="Till-Hendrik Macher",
     author_email="macher@uni-trier.de",
     description="Advanced Pipeline for Simple yet Comprehensive AnaLysEs of DNA metabarcoding data - Conda installer (workaround)",
